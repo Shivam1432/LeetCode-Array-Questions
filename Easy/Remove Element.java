b433ad4c15cdba7b2c47b1ba2e6560fd73d2096a
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-03-16 18:45:19 +0530

    Update Remove Element.java

--- Easy/Remove Element.java
+++ Easy/Remove Element.java
@@ -1,29 +1,11 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int count=0,temp;
-        for(int i=0;i<nums.length;i++)
+         int counter = 0;
+        for (int i = 0; i < nums.length; i++) 
         {
-            if(nums[i]!=val)
-            {
-                count++;
-            }
-            else
-            {
-                nums[i]=0;
-            }
+            if (nums[i] != val) 
+                nums[counter++] = nums[i];
         }
-        for(int i=0;i<nums.length;i++)
-        {
-            for(int j=i+1;j<nums.length;j++)
-            {
-                if(nums[i]<nums[j])
-                {
-                    temp=nums[i];
-                    nums[i]=nums[j];
-                    nums[j]=temp;
-                }
-            }
-        }
-        return count;
+        return counter;
     }
 }
